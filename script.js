d413fb285b6980c6e97e5d8c930e78624b2da70c
Maksim Genyush <maxlindrgen0799@mail.ru>
2020-04-18 21:37:59 +0300

    не работает

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
 'use strict';
+
+const todoControl = document.querySelector('.todo-control'),
+      headerInput = document.querySelector('.header-input'),
+      todoList  = document.querySelector('.todo-list '),
+      todoCompleted = document.querySelector('.todo-completed');
+
+
+const todoData = [];
+
+const render = function() {
+    todoList.textContent = '';
+    todoCompleted.textContent = '';
+
+    todoData.forEach(function(item){
+        const li = document.createElement('li');
+        li.classList.add('todo-item');
+
+        li.innerHTML = '<span class="text-todo">' + item.value +  '</span>' + 
+            '<div class="todo-buttons">' + 
+                '<button class="todo-remove"></button>' + 
+                '<button class="todo-complete"></button>' + 
+            '</div>';
+
+        if(item.completed){
+            todoCompleted.append(li);
+        } else{
+            todoList.append(li);
+        }
+
+            todoList.append(li);
+        const btnTodoCompleted = li.querySelector('.todo-complete');
+
+        btnTodoCompleted.addEventListener('click', function(){
+            item.completed = !item.completed;
+            render();
+        })
+    
+        });
+};
+
+todoControl.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const newTodo = {
+        value: headerInput.value,
+        completed: false
+    };
+    // if(newTodo.value !== ''){
+    // todoData.push(newTodo);
+    // newTodo.value.textContent = '';
+    // };  
+    todoData.push(newTodo);  
+    render();
+});
+
+render();
