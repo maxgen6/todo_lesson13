4ccef4d80603b4434079759a2d8f3137398c2d88
Maksim Genyush <maxlindrgen0799@mail.ru>
2020-04-19 23:50:47 +0300

    не работает полностью

--- script.js
+++ script.js
@@ -4,9 +4,15 @@ const todoControl = document.querySelector('.todo-control'),
       headerInput = document.querySelector('.header-input'),
       todoList  = document.querySelector('.todo-list '),
       todoCompleted = document.querySelector('.todo-completed');
+      
 
 
-const todoData = [];
+const todoData = [] || JSON.parse(localStorage.getItem('memory')) ;
+    // localStorage.setItem('memory', JSON.stringify(todoData));
+
+// let jsonToDoData = JSON.stringify(todoData);
+//     localStorage.setItem('memory', jsonToDoData);
+
 
 const render = function() {
     todoList.textContent = '';
@@ -15,19 +21,24 @@ const render = function() {
     todoData.forEach(function(item){
         const li = document.createElement('li');
         li.classList.add('todo-item');
-
+        console.log(li);
+        li.value = localStorage.getItem('memory');
         li.innerHTML = '<span class="text-todo">' + item.value +  '</span>' + 
             '<div class="todo-buttons">' + 
                 '<button class="todo-remove"></button>' + 
                 '<button class="todo-complete"></button>' + 
             '</div>';
-
-        if(item.completed){
+        
+        console.log();
+       if(item.completed){
             todoCompleted.append(li);
         } else{
             todoList.append(li);
         }
-
+        
+        
+        // item.value = localStorage.getItem('memory');
+        // console.log(item);
            
         const btnTodoCompleted = li.querySelector('.todo-complete');
 
@@ -42,8 +53,9 @@ const render = function() {
             li.remove();
            
         });
-    
+        
         });
+       
 };
 
 todoControl.addEventListener('submit', function(event){
@@ -56,12 +68,21 @@ todoControl.addEventListener('submit', function(event){
     if(newTodo.value !== ''){
     todoData.push(newTodo);
     
-    };  
-    headerInput.textContent = '';
-    console.log(newTodo.value);
+    }  
+    localStorage.setItem('memory', JSON.stringify(todoData));
+    // const todoData = localStorage.getItem('memory');
+    // console.log(newTodo.value);
     headerInput.value = '';
-    // todoData.push(newTodo);  
+    // let jsonToDoData = JSON.stringify(todoData);
+    // console.log(jsonToDoData);
+    // localStorage.setItem('memory', jsonToDoData);
+    // todoData = JSON.parse(jsonToDoData);
+   
     render();
 });
-
+// showText();
 render();
+
+  // todoData = localStorage.getItem('memory');
+    // showText();
+    // todoData.push(newTodo); 
